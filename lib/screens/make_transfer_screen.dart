d200103585a440e7d316c419c91c2e639ffe1acd
Farida Yasser <farida.yasser510@gmail.com>
2023-10-08 10:32:49 +0300

    second commit

--- lib/screens/make_transfer_screen.dart
+++ lib/screens/make_transfer_screen.dart
@@ -3,6 +3,7 @@ import 'package:banking_system/controllers/transfer_controller.dart';
 import 'package:banking_system/database/transfer_db.dart';
 import 'package:banking_system/database/user_db.dart';
 import 'package:banking_system/screens/home_screen.dart';
+import 'package:banking_system/screens/view_customer_details.dart';
 import 'package:banking_system/screens/view_past_transfers_screen.dart';
 import 'package:banking_system/widgets/navigation_button.dart';
 import 'package:banking_system/widgets/user_card_widget.dart';
@@ -104,169 +105,181 @@ class _MakeTransfersScreenState extends State<MakeTransfersScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text("Transfer Money to " + widget.receiverData.name, style: appbarNameTitleTextStyle, ),
-      ),
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              SizedBox(height: 30,),
-              // transfer info
-              Padding(
-                padding: const EdgeInsets.only(top: 20),
-                child: Row(
-                  children: [
-                    //sender info
+    return WillPopScope(
+      onWillPop: () async{
+        Get.to(ViewCustomerDetails(
+          selectedCustomer: await UserDB().fetchUserById(widget.receiverData.user_id),
+          currentUser: await UserDB().fetchUserById(widget.senderData.user_id),
+          pastTransfersList: await TransferDB().fetchTransfersBySenderAndReceiverId(widget.senderData.user_id, widget.receiverData.user_id),
 
-                   Column(children: [
-                     UserCardWidget(userData: widget.senderData, title: "Sender"),
+        ));
+        return false;
 
-                     GetBuilder<TransferController>(
-                       init: TransferController(),
-                       builder: (controller) {
-                         return Text(transferController.senderBalance.toStringAsFixed(2) + " EGP",style: balanceStyle);
-                       },
-                     )
-                   ],),
-
-                    SizedBox(width: 15,),
-
-                    //transfer icon
-
-                    Icon(Icons.arrow_forward_outlined, size: 35,),
-
-                    SizedBox(width: 15,),
-
-                    //receiver info
-
-                    Column(children: [
-                      UserCardWidget(userData: widget.receiverData, title: "Receiver"),
-
-
-
-                      GetBuilder<TransferController>(
-
-                        init: TransferController(),
-
-                        builder: (controller) {
-                          return Text(transferController.receiverBalance.toStringAsFixed(2) + " EGP", style: balanceStyle,);
-                        },
-                      )
-                    ],),
-
-
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 50,),
-
-              //transfer form
-              Padding(
-                padding: const EdgeInsets.fromLTRB(45, 20, 20, 20),
-                child: Form(
-                  key: _formKey,
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text("Transfer Money to " + widget.receiverData.name, style: appbarNameTitleTextStyle, ),
+        ),
+        body: SingleChildScrollView(
+          child: SafeArea(
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+                // transfer info
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
                   child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      TextFormField(
-                        controller: amountOfTransferController,
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        decoration: const InputDecoration(
-                            hintText: "Amount",
-                            alignLabelWithHint: true,
-                            constraints: BoxConstraints(
-                                maxWidth: 200
-                            ),
-                            border: OutlineInputBorder()
-                        ),
+                      //sender info
 
-                        validator: (value) {
-                          if(double.parse(value.toString()) > widget.senderData.balance){
-                            return "There is not enough \n money in your balance";
-                          }
-                          if(double.parse(value.toString()) < 0){
-                            return "You cannot enter\n an amount in negative";
-                          }
-                          return null;
-                        },
+                     Column(children: [
+                       UserCardWidget(userData: widget.senderData, title: "Sender"),
 
-                      ),
+                       GetBuilder<TransferController>(
+                         init: TransferController(),
+                         builder: (controller) {
+                           return Text(transferController.senderBalance.toStringAsFixed(2) + " EGP",style: balanceStyle);
+                         },
+                       )
+                     ],),
 
-                      SizedBox(width: 30,),
+                      SizedBox(width: 15,),
 
-                      Container(
-                        child: Padding(
-                          padding: const EdgeInsets.only(bottom: 5),
-                          child: Material(
-                            color: DarkBlueColor,
-                            child: InkWell(
-                              onTap: () async{
+                      //transfer icon
+
+                      Icon(Icons.arrow_forward_outlined, size: 35,),
+
+                      SizedBox(width: 15,),
+
+                      //receiver info
+
+                      Column(children: [
+                        UserCardWidget(userData: widget.receiverData, title: "Receiver"),
 
 
-                                if (_formKey.currentState!.validate()) {
-                                  await makeTransfer();
 
-                                }
+                        GetBuilder<TransferController>(
 
-                              },
-                              child: Padding(
-                                padding: const EdgeInsets.all(10),
-                                child: Text("Transfer", style: buttonTextStyle,),
-                              ),
-                            ),
-                          ),
-                        ),
-                      )
+                          init: TransferController(),
+
+                          builder: (controller) {
+                            return Text(transferController.receiverBalance.toStringAsFixed(2) + " EGP", style: balanceStyle,);
+                          },
+                        )
+                      ],),
+
 
                     ],
                   ),
                 ),
-              ),
 
-              SizedBox(height: 30,),
+                SizedBox(height: 50,),
+
+                //transfer form
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(45, 20, 20, 20),
+                  child: Form(
+                    key: _formKey,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextFormField(
+                          controller: amountOfTransferController,
+                          textAlign: TextAlign.center,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                              hintText: "Amount",
+                              alignLabelWithHint: true,
+                              constraints: BoxConstraints(
+                                  maxWidth: 200
+                              ),
+                              border: OutlineInputBorder()
+                          ),
+
+                          validator: (value) {
+                            if(double.parse(value.toString()) > widget.senderData.balance){
+                              return "There is not enough \n money in your balance";
+                            }
+                            if(double.parse(value.toString()) < 0){
+                              return "You cannot enter\n an amount in negative";
+                            }
+                            return null;
+                          },
+
+                        ),
+
+                        SizedBox(width: 30,),
+
+                        Container(
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 5),
+                            child: Material(
+                              color: DarkBlueColor,
+                              child: InkWell(
+                                onTap: () async{
+
+
+                                  if (_formKey.currentState!.validate()) {
+                                    await makeTransfer();
+
+                                  }
+
+                                },
+                                child: Padding(
+                                  padding: const EdgeInsets.all(10),
+                                  child: Text("Transfer", style: buttonTextStyle,),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30,),
 
 
 
 
 
 
-              NavigationButton(onPressed: () async{
-                User user = await widget.senderData;
-                List<User> users = await UserDB().fetchAllUsers();
-                users.removeAt(0);
+                NavigationButton(onPressed: () async{
+                  User user = await widget.senderData;
+                  List<User> users = await UserDB().fetchAllUsers();
+                  users.removeAt(0);
 
-                Get.to(()=>HomeScreen(currentUser: UserDB().fetchUserById(1),));
-              }, text: "Back to Home Page"),
+                  Get.to(()=>HomeScreen(currentUser: UserDB().fetchUserById(1),));
+                }, text: "Back to Home Page"),
 
-              SizedBox(height: 10,),
-
-
-              GetBuilder(
-                init: TransferController(),
-                builder: (controller) {
-                  if(transferController.transfersList.length == 0){
-                    return Container();
-                  }
-                  return NavigationButton(onPressed: () async{
-
-                    Get.to(PastTransfersListScreen(
-
-                      currentUser: await UserDB().fetchUserById(widget.senderData.user_id),
-                      selectedCustomer: await UserDB().fetchUserById(widget.receiverData.user_id),
-                      pastTransfersList: widget.pastTransfersList,
-                    ));
-                  }, text: "View Transfers History");
-                },
-              )
+                SizedBox(height: 10,),
 
 
+                GetBuilder(
+                  init: TransferController(),
+                  builder: (controller) {
+                    if(transferController.transfersList.length == 0){
+                      return Container();
+                    }
+                    return NavigationButton(onPressed: () async{
 
-            ],
+                      Get.to(PastTransfersListScreen(
+
+                        currentUser: await UserDB().fetchUserById(widget.senderData.user_id),
+                        selectedCustomer: await UserDB().fetchUserById(widget.receiverData.user_id),
+                        pastTransfersList: widget.pastTransfersList,
+                      ));
+                    }, text: "View Transfers History");
+                  },
+                )
+
+
+
+              ],
+            ),
           ),
         ),
       ),
